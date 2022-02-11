a229131055e6583ac7ac592181292c3234dc4098
Rushikesh <rushikesh@bigscal.com>
2022-02-11 11:12:23 +0530

    complete Ipc

--- IPC/main.js
+++ IPC/main.js
@@ -3,13 +3,32 @@ const electron = require("electron");
 
 const { app, dialog } = electron;
 
-const ipc = electron.ipcRenderer
+const ipc = electron.ipcMain
 
 app.on("ready", ()=>{
-    const mainWindow = new BrowserWindow({})
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
     mainWindow.loadFile("index.html")
 
 })
-ipc.on("open-error-dialog", ()=>{
+
+// for dialog
+ipc.on("open-error-dialog", (event)=>{
     dialog.showErrorBox("As a error Message", "Demo error massage")
+    event.sender.send("opned-error-massage", "Main process opened the error dialog")
+})
+
+// for async operations
+ipc.on("async-reply", (event)=>{
+    console.log("async-reply called", event)
+    event.sender.send("async-replied", "async-reply called from main Renderer")
+})
+
+// for sync operation
+ipc.on("sync-message", (event)=>{
+    event.returnValue = 'sync-reply'
 })
