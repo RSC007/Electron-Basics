24272c3cd75337e4623e7bce30ae00ac5083e4ad
pavangandhi9197 <pavangandhi9197@gmail.com>
2022-02-14 10:55:11 +0530

    Modify json for window and add new functionlites like add new window

--- Electron-React/electron-timer/public/Electron/electron.js
+++ Electron-React/electron-timer/public/Electron/electron.js
@@ -1,8 +1,10 @@
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
 
-const { app, BrowserWindow, dialog, ipcMain } = require('electron');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow, dialog, ipcMain } = require("electron");
+const isDev = require("electron-is-dev");
 
+let capturePage = null;
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
@@ -12,20 +14,22 @@ function createWindow() {
       nodeIntegration: false, // is default value after Electron v5
       contextIsolation: true, // protect against prototype pollution
       enableRemoteModule: false, // turn off remote
-      preload: path.join(__dirname, "./preload.js") // use a preload script
+      preload: path.join(__dirname, "./preload.js"), // use a preload script
     },
   });
+
+  capturePage = win.webContents.capturePage();
 
   // and load the index.html of the app.
   // win.loadFile("index.html");
   win.loadURL(
     isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
   );
   // Open the DevTools.
   if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' });
+    // win.webContents.openDevTools({ mode: 'detach' });
   }
 }
 
@@ -37,19 +41,38 @@ app.whenReady().then(createWindow);
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-console.log("Occured")
-ipcMain.on('notify', (_, message) => {
-  dialog.showErrorBox("Demo Message", message)
-})
+ipcMain.on("notify", (_, message) => {
+  dialog.showErrorBox("Demo Message", message);
+});
+
+// create-window
+ipcMain.on("create-window", (_, title) => {
+  console.log("title", title);
+  createWindow();
+});
+
+ipcMain.on("close-all-window", () => {
+  app.quit();
+});
+
+ipcMain.on("take-screenshot", () => {
+  capturePage.then((image) => {
+    //writing  image to the disk
+    fs.writeFile(`test.png`, image.toPNG(), (err) => {
+      if (err) throw err;
+      console.log("Image Saved");
+    });
+  });
+});
--- Electron-React/electron-timer/public/Electron/preload.js
+++ Electron-React/electron-timer/public/Electron/preload.js
@@ -1,10 +1,25 @@
-const { contextBridge } = require('electron');
-const ipc = require('electron').ipcRenderer
+const { contextBridge } = require("electron");
+const ipc = require("electron").ipcRenderer;
 
-contextBridge.exposeInMainWorld('electron', {
-    notificationApi: {
-        sendNotification(message) {
-          ipc.send('notify', message);
-        }
-      },
+contextBridge.exposeInMainWorld("electron", {
+  notificationApi: {
+    sendNotification(message) {
+      ipc.send("notify", message);
+    },
+  },
+  newWindowCreateApi: {
+    createNewWindow(title) {
+      ipc.send("create-window", title);
+    },
+  },
+  closeAllWindowApi: {
+    closeAllWindow() {
+      ipc.send("close-all-window");
+    },
+  },
+  takeScreenShotApi: {
+    takeScreenShot() {
+      ipc.send("take-screenshot");
+    },
+  },
 });
